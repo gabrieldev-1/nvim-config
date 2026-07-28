0d8a9711be73f3e4c1328dee51ac3249fb42b118
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-07-28 20:24:48 -0300

    options file initial configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,21 @@
 local opt = vim.opt
 
+-- Numbering and interface
 opt.number = true
 opt.relativenumber = true
+opt.cursorline = true
 
+-- Indentation and Tabulation
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+opt.wrap = true
 
+-- colors
 opt.termguicolors = true
+
+-- search
 opt.ignorecase = true
 opt.smartcase = true
+opt.hlsearch = true
+
