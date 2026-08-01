61730b71f5b9b7995f98a1b0a89f70a8bb3ed2a5
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-07-31 21:04:49 -0300

    LSP for C (clangd) added and configured in the plugins/lsp.lua file.

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,1 +1,61 @@
-return { }
+return {
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      -- 1. Inicializa o Mason
+      require("mason").setup()
+
+      -- 2. Garante que os servidores estejam instalados
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" , "clangd"},
+      })
+
+      -- 3. Mapeamentos de teclas no evento LspAttach (mantém igual)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        end,
+      })
+
+      -- 4. NOVO PADRÃO: Configuração e ativação usando a API nativa vim.lsp
+      -- Em vez de require('lspconfig').lua_ls.setup({...}), fazemos:
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      -- Ativa o servidor habilitado para o Neovim
+      vim.lsp.enable('lua_ls')
+      
+      vim.lsp.config('clangd', {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+        },
+      })
+      vim.lsp.enable('clangd')
+    end,
+  },
+}
